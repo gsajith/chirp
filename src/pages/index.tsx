c8a36ad7ce067eb933c7dd4ce5cef6e70ddbafbe
Gautham Sajith <gsajith@umich.edu>
2023-11-16 20:44:51 -0500

    Post router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,12 @@ import Head from "next/head";
 
 import styles from "./index.module.css";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { api } from "~/utils/api";
 
 export default function Home() {
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
+
   return (
     <>
       <Head>
@@ -16,6 +19,11 @@ export default function Home() {
         <div>
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
       </main>
     </>
