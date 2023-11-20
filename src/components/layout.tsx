f3e09800ab53ecbd0ab730d3e4b41e23ea119cc6
Gautham Sajith <gsajith@umich.edu>
2023-11-19 22:26:55 -0500

    Fix eslint issues

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
-import { PropsWithChildren } from "react";
+import { type PropsWithChildren } from "react";
 import styles from "./components.module.css";
 
-export const PageLayout = (props: PropsWithChildren<{}>) => {
+export const PageLayout = (props: PropsWithChildren<object>) => {
   return (
     <main className={styles.main}>
       <div className={styles.mainContent}>{props.children}</div>
